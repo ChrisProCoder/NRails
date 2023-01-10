e1948f9ebfccc4b8f6aafae7c480b9bfdb5c97d4
Chris White <contactchrisw@proton.me>
2023-01-10 14:45:52 +0000

    Added Added contact form view, route, controller and rails form

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
