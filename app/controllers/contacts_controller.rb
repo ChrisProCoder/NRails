3101a7bb0fced6bb87ac6b594a36c2a3f4b0c88a
Chris White <contactchrisw@proton.me>
2023-01-13 17:23:40 +0000

    Added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,19 @@
-class ContactsController < ApplicationController 
+class ContactsController < ApplicationController
   def new 
-    
+    @contact = Contact.new 
+  end 
+  def create 
+    @contact = Contact.new(contact_params)
+    if @contact.save 
+      redirect_to new_contact_path, notice:  "Message Sent"
+    else 
+      redirect_to new_contact_path, alert:  "An error ocurrred, please ty again" 
+    end
   end
+ 
+
+  private 
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
